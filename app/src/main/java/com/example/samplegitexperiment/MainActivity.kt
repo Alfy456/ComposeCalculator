6a87baba9c099dccfbed81d849bfe45b5ddd9b13
Alfy524 <alfy524@gmail.com>
2023-09-17 17:57:23 -0400

    change 1

--- app/src/main/java/com/example/samplegitexperiment/MainActivity.kt
+++ app/src/main/java/com/example/samplegitexperiment/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
-                    print("HAI")
+                    print("Change 1")
                 }
             }
         }
