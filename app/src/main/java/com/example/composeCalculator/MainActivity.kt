064587f7b363c85e8e65c6b89cfe2bf22f2956f7
Alfy524 <alfy524@gmail.com>
2023-09-26 21:29:33 -0400

    Skeleton of the Calculator using Jetpack Compose is completed

--- app/src/main/java/com/example/composeCalculator/MainActivity.kt
+++ app/src/main/java/com/example/composeCalculator/MainActivity.kt
@@ -3,45 +3,42 @@ package com.example.composeCalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.composeCalculator.ui.theme.MediumGrey
 import com.example.composeCalculator.ui.theme.SampleGitExperimentTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SampleGitExperimentTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                    print("Change 1")
-                }
-            }
+            val viewModel = viewModel<CalculatorViewModel>()
+            val state = viewModel.state
+            val buttonSpacing = 8.dp
+            Calculator(
+                state = state,
+                onAction = viewModel::onAction,
+                buttonSpacing = buttonSpacing,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MediumGrey)
+                    .padding(16.dp)
+            )
+
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SampleGitExperimentTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/example/composeCalculator/ui/theme/Color.kt
+++ app/src/main/java/com/example/composeCalculator/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val MediumGrey = Color(0xFF4D4D4D)
+val LightGrey = Color(0xFF808080)
+val Orange = Color(0xFFE64A19)
